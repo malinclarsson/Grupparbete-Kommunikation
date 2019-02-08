89dfa11c5a89657c06455112dfd9b759c7abd31b
Malin Larsson <malin.larsson@utb.ecutbildning.se>
2019-02-08 09:40:04 +0100

    item works

--- Malin/item.js
+++ Malin/item.js
@@ -1,35 +1,31 @@
-/* 
-Items:
-    1. title
-    2. description
-    3. description icon
-    4. add new item
-    5. new item icon
-    6. remove item
-    7. remove item icon
-*/
-const list_background = document.querySelector('.list__background');
-const list_item = document.querySelector('.list-item');
-const form = document.querySelector('.form-title');
-const textInput = document.querySelector('.form-title__input');
-const addButton = document.querySelector('.button-add');
-const todo_list = document.querySelector('.todo-list');
+const form = document.querySelector('#form');
+const textInput = document.querySelector('#text');
+const addButton = document.querySelector('#add');
+const todos = document.querySelector('#todos');
+
 const todoHistory = [];
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const text = textInput.value;
+
   if (text.length > 0) {
     const todo = document.createElement('li');
     todo.classList.add('todo');
     todo.textContent = text;
-    todo.addEventListener('click', () => {
+
+    const iconRemove = document.createElement('i');
+    todo.classList.add('todo');
+    iconRemove.textContent = "remove";
+
+    todo.addEventListener('click', () => { // iconRemove
       todo.remove();
     });
+
     todoHistory.push(todo);
     textInput.value = '';
     textInput.focus();
     todos.appendChild(todo);
-    list_background.appendChild(list_item);
   }
+  
 });
