e0c32c30ce133a86a70035fe9c18b9e1b09c211c
Malin Larsson <malin.larsson@utb.ecutbildning.se>
2019-02-11 10:06:23 +0100

    files sorted

---
+++ Rickard/drag and drop.js
@@ -0,0 +1,13 @@
+let id ;
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function dragStart(ev) {
+  id = ev.target.id;
+}
+
+function drop(ev) {
+  ev.target.append(document.getElementById('id'));
+}
